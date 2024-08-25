7eff8b73b5c5ff72a822d6c0dfbd6070a782ad4c
cooleverest <33816939+cooleverest@users.noreply.github.com>
2024-08-25 19:55:21 +1000

    allowed fetch by date to return all data for a day

--- components/HandleAPI.mjs
+++ components/HandleAPI.mjs
@@ -1,10 +1,10 @@
 // this is mjs during development
 import React from "react";
 
-const SERVER_IP = '35.197.170.143'
+const SERVER_IP = '34.151.124.22'
 const URL = 'http://' + SERVER_IP + ':4000/api' // pi is test url
 
-const fetchData = async (start = null, end = null) => {
+const fetchData = async (start = null, end = null, date = null) => {
     // if start and end dates are provided
     if (start && end) { 
         try {
@@ -26,10 +26,29 @@ const fetchData = async (start = null, end = null) => {
             console.log({error: error.message})
         }
 
+    } else if (date) {
+        try  {
+            const req = URL + '?date=' + date
+
+            const response = await fetch(req, {
+                method: "GET",
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            })
+
+            if (!response.ok) {
+                throw new Error(`bad response: ${response.status}`)
+            } else {
+                return await response.json()
+            }
+        } catch (error) {
+            console.log({error: error.message})
+        }
     }
     
 };
 
-const out = await fetchData()
+const out = await fetchData(null,null,'2006-12-17')
 
 console.log(out)
