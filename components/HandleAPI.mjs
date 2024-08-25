7b2ba77138a311bb7d8349756038ef7a5a824e2c
cooleverest <33816939+cooleverest@users.noreply.github.com>
2024-08-25 20:01:11 +1000

    seperated fetching date and date range

--- components/HandleAPI.mjs
+++ components/HandleAPI.mjs
@@ -4,51 +4,50 @@ import React from "react";
 const SERVER_IP = '34.151.124.22'
 const URL = 'http://' + SERVER_IP + ':4000/api' // pi is test url
 
-const fetchData = async (start = null, end = null, date = null) => {
+const fetchData = async (start, end) => {
     // if start and end dates are provided
-    if (start && end) { 
-        try {
-            // api performs error checking on dates
-            const req = URL + '?start=' + start + "&end=" + end
+    try {
+        // api performs error checking on dates
+        const req = URL + '?start=' + start + "&end=" + end
 
-            const response = await fetch(req, {
-                    method: "GET",
-                    headers: {
-                        'Content-Type': 'application/json' // to prevent 'undefined'
-                    }
-                })
-            if (!response.ok) {
-                throw new Error(`bad response: ${response.status}`)
-            } else {
-                return await response.json() // api should always give json
-            }
-        } catch (error) {
-            console.log({error: error.message})
-        }
-
-    } else if (date) {
-        try  {
-            const req = URL + '?date=' + date
-
-            const response = await fetch(req, {
+        const response = await fetch(req, {
                 method: "GET",
                 headers: {
-                    'Content-Type': 'application/json'
+                    'Content-Type': 'application/json' // to prevent 'undefined'
                 }
             })
-
-            if (!response.ok) {
-                throw new Error(`bad response: ${response.status}`)
-            } else {
-                return await response.json()
-            }
-        } catch (error) {
-            console.log({error: error.message})
+        if (!response.ok) {
+            throw new Error(`bad response: ${response.status}`)
+        } else {
+            return await response.json() // api should always give json
         }
+    } catch (error) {
+        console.log({error: error.message})
     }
-    
 };
 
-const out = await fetchData(null,null,'2006-12-17')
+const fetchDate = async (date) => {
+    try  {
+        const req = URL + '?date=' + date
+
+        const response = await fetch(req, {
+            method: "GET",
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+
+        if (!response.ok) {
+            throw new Error(`bad response: ${response.status}`)
+        } else {
+            return await response.json()
+        }
+    } catch (error) {
+        console.log({error: error.message})
+    }
+}
+
+//const out = await fetchData(null,null)
+const out = await fetchDate('2006-12-17')
 
 console.log(out)
